70925dc9cfd5610b57ab3bb9fbc1684e25b4fff4
Arvind Thangamani <dnivra26@gmail.com>
2018-03-19 12:38:01 +0530

    add logs

--- main.go
+++ main.go
@@ -8,12 +8,15 @@ import (
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	log.Println("Handling root request")
 	fmt.Fprintf(w, time.Now().String()) // send data to client side
+	log.Println("Request completed")
 }
 
 func main() {
 	http.HandleFunc("/", sayhelloName)       // set router
 	err := http.ListenAndServe(":9091", nil) // set listen port
+	log.Println("App listening on 9091")
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
